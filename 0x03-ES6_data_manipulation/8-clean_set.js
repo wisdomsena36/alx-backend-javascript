d21461cdf06a5d33c8a757d97e6bc87caba28407
Wisdom Sena <wisdomsena36@gmail.com>
2024-04-16 14:46:35 -0700

    Fix 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
+  if (!startString || typeof startString !== 'string' || typeof set !== 'object' || !(set instanceof Set)) return '';
   return [...set]
-    .filter((el) => el.startsWith(startString))
+    .filter((el) => typeof el === 'string' && el.startsWith(startString))
     .map((el) => el.slice(startString.length))
     .join('-');
 }
