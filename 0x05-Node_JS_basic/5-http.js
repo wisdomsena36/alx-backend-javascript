dae9654355f94f96b3af06b4f5983f3209acd975
Wisdom Sena <wisdomsena36@gmail.com>
2024-06-04 15:38:16 -0700

    Fix requirement for 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,6 +3,12 @@ const students = require('./3-read_file_async');
 const hostname = '127.0.0.1';
 const port = 1245;
 
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ * @author Wisdom Sena <https://github.com/wisdomsena36>
+ */
+
 const app = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
